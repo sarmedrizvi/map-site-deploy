c927da18754e812783948181151c5aa45e9adc06
sarmedrizvi <sarmedrizvi@gmail.com>
2020-04-14 21:11:16 +0500

    Production

--- .nuxt/store.js
+++ .nuxt/store.js
@@ -10,10 +10,6 @@ let store = {};
 (function updateModules () {
   // If store is an exported method = classic mode (deprecated)
 
-  if (typeof store === 'function') {
-    return console.warn('Classic mode for store/ is deprecated and will be removed in Nuxt 3.')
-  }
-
   // Enforce store modules
   store.modules = store.modules || {}
 
@@ -21,19 +17,6 @@ let store = {};
   resolveStoreModules(require('..\\store\\modules\\markerData.js'), 'modules/markerData.js')
 
   // If the environment supports hot reloading...
-
-  if (process.client && module.hot) {
-    // Whenever any Vuex module is updated...
-    module.hot.accept([
-      '..\\store\\store.js',
-      '..\\store\\modules\\markerData.js',
-    ], () => {
-      // Update `root.modules` with the latest definitions.
-      updateModules()
-      // Trigger a hot update in the store.
-      window.$nuxt.$store.hotUpdate(store)
-    })
-  }
 })()
 
 // createStore
